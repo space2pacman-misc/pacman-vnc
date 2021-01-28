b5566b85cd21d1806aff79cc4549e38391ceda13
space2pacman <space2pacman@gmail.com>
2021-01-28 03:33:45 +0300

    move headers to callback

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let fs = require("fs");
 let express = require("express");
 let app = express();
 
-//let file = fs.createWriteStream("tcp.mp4");
+let file = fs.createWriteStream("tcp.mp4");
 let server = net.createServer(socket => {
 	socket.on("data", data => {
 		file.write(data);
@@ -12,12 +12,13 @@ let server = net.createServer(socket => {
 
 server.listen(4444, "127.0.0.1");
 
-let headers = {
-	"Content-Length": fs.statSync("tcp.mp4").size,
-	"Content-Type": "video/mp4"
-}
 
 app.get("/stream", (req, res) => {
+	let headers = {
+		"Content-Length": fs.statSync("tcp.mp4").size,
+		"Content-Type": "video/mp4"
+	}
+	
 	res.writeHead(200, headers);
 
 	fs.createReadStream("tcp.mp4").pipe(res);
